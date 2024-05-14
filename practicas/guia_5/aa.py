1733aef3539a888d96afde2ca2b97f2dd1f47210
Pablo Mercado <pablomercado0123@gmail.com>
2024-05-13 23:55:36 -0300

    realiza hasta ej8 p5

---
+++ practicas/guia_5/aa.py
@@ -0,0 +1,20 @@
+import numpy as np
+import numpy.linalg as lng
+
+# A = np.array([[1,1,1],[1,1,1],[1,1,1]])
+# x = np.array([-1,1,0])
+# x2 = np.array([-1,0,1])
+# x3 = np.array([1,1,1])
+
+
+A = np.array([[1,-1,1],[-2,4,-2],[0,-1,1]])
+x = np.array([-1,1,0])
+x2 = np.array([-1,0,1])
+x3 = np.array([1,1,1])
+
+print(A @ x)
+
+# res = lng.solve(A, np.array([0,0,0]))
+
+
+
